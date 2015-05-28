12aaccbaa8a01a958f3be634c562cf0ab0e30313
mark law <markclaw91@gmail.com>
2015-05-27 21:51:04 -0400

    first commit

---
+++ test.go
@@ -0,0 +1,19 @@
+// test
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	fmt.Println("Hello World!")
+	
+	
+	
+	fmt.Println(ErfInv(0.1))
+	fmt.Println(InverserNormalCDF(2))
+	fmt.Println(InverseNormalDist(0.5, 1, 2))
+	fmt.Println(InverseNormalDist(0.4, 1, 4))
+	fmt.Println(InverseNormalDist(0.1, 0.1, 0.12))
+}
